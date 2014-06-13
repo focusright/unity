4a8c5ab0b409aaf7f0aea88f76e1158c077b74ba
mlee <mlee@nexon.net>
2014-06-12 22:37:03 -0700

    2d Sprite Punch
    Custom sprite sheet made from Sprite Something on IPad

---
+++ tests/2d-sprite-punch/Assets/CharacterScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterScript : MonoBehaviour {
+
+	public Animator anim;
+
+	public AudioSource punchHit;
+
+	public AudioSource swingSound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space)) { 
+			this.anim.Play( "Punch" );
+		}
+	
+	}
+
+	void PlayPunchHitSound() {
+
+		punchHit.Play();
+
+	}
+
+	void PlaySwingSound() {
+
+		swingSound.Play();
+
+	}
+
+
+}
